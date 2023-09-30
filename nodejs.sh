10a4f3b47557c36edf4f675d75a7588362629d85
Atikant Supapanya <feineirz@live.com>
2023-09-30 23:36:57 +0700

    Checkpoint

--- nodejs.sh
+++ nodejs.sh
@@ -438,7 +438,7 @@ const storage = multer.diskStorage({
         }
     },
     filename: (req, file, cb) => {
-        cb(null, `file-${Date.now()}${path.extname(file.originalname)}`)
+        cb(null, `file-\${Date.now()}\${path.extname(file.originalname)}`)
     },
 })
 
